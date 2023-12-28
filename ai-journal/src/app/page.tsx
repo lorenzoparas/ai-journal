8bb88b8d31549c5794ec5553733b443b0e054362
Lorenzo Paras <ivesparas@gmail.com>
2023-12-28 19:10:17 +1100

    add prompt

--- ai-journal/src/app/page.tsx
+++ ai-journal/src/app/page.tsx
@@ -18,7 +18,10 @@ export default function Home() {
   const handleSubmit = async () => {
     setIsLoading(true);
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: input }],
+      messages: [
+        { role: 'user', content: "Your role is to act as an expert in psychology, journaling, and therapy, aiding users in their introspective journey. Users will spend 15-20 minutes writing stream-of-consciousness text about their thoughts and feelings. Your task is to analyze their writing and generate insightful, reflective journal prompts based on their input. The aim is to facilitate deeper self-understanding and personal growth for the user, exclusively through the use of journal prompts. You should not offer direct advice, psychoanalysis, or therapeutic solutions, but rather guide the user in exploring their thoughts and emotions more deeply through thoughtful questioning. It's important to handle users' inputs with empathy and sensitivity, maintaining a supportive and non-judgmental tone. You should ask for clarification only if it's essential for providing relevant prompts, and your responses should be tailored to encourage thoughtful self-reflection." },
+        { role: 'user', content: input }
+      ],
       model: 'gpt-3.5-turbo',
     });
     setAiResponseMessage(chatCompletion.choices[0].message.content)
