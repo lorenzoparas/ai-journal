9d6c2e0dca6a7059fe554a02bf2983a7e9aadaf1
Lorenzo Paras <ivesparas@gmail.com>
2024-03-04 19:15:56 +1100

    [AIJ-9] clean up

--- ai-journal/src/app/components/JournalEntry.tsx
+++ ai-journal/src/app/components/JournalEntry.tsx
@@ -6,7 +6,6 @@ import './JournalEntry.css'
 
 const JournalEntry = () => {
     const { journal, setJournal, serviceOutput: { thinkingPatterns = [] } } = useContext<JournalContextType>(JournalContext);
-    const [journalOutput, setJournalOutput] = useState('');
 
     const resultText = () => {
         let journalCopy = journal;
@@ -35,41 +34,10 @@ const JournalEntry = () => {
                     rows={24}
                     placeholder="What's on your mind?"
                     className="whitespace-pre-wrap focus:outline-none focus:ring-0 w-full" 
-                /> : <div className="font-sans" dangerouslySetInnerHTML={{ __html: resultText() }} />}
-                    
-                    {/* <Textarea
-                    onChange={e => setJournal(e.target.value)}
-                    rows={24}
-                    variant='static'
-                    placeholder="what's on your mind?"
-                    size="lg"
-                /> */}
+                /> : <div className="font-sans" dangerouslySetInnerHTML={{ __html: resultText() }} /> }
             </div>
         </Card>
     );
 };
 
 export default JournalEntry
-
-// [
-//     {
-//         "quote": "Sometimes I don't know why I even try so hard.",
-//         "thinkingPattern": "Mind reading",
-//         "explanation": "This is an example of mind reading, assuming you know what others are thinking or feeling without any evidence. A more helpful reframe would be to communicate directly with others to understand their perspectives and not make assumptions about their intentions."
-//     },
-//     {
-//         "quote": "There are so many times when other people try to hurt me.",
-//         "thinkingPattern": "Overgeneralisation",
-//         "explanation": "This is an example of overgeneralisation, assuming that because some people have hurt you, everyone will hurt you. A more helpful reframe would be to recognize that not everyone will behave the same way and to address specific instances of hurtful behavior individually."
-//     },
-//     {
-//         "quote": "I guess I just gotta let it go.",
-//         "thinkingPattern": "Fortune telling",
-//         "explanation": "This is an example of fortune telling, predicting that nothing will change or improve in the future. A more helpful reframe would be to focus on taking positive actions to address the situation and create change rather than assuming nothing can be done."
-//     },
-//     {
-//         "quote": "I don't even know what to do myself.",
-//         "thinkingPattern": "Catastrophising",
-//         "explanation": "This is an example of catastrophising, imagining the worst-case scenario and feeling overwhelmed. A more helpful reframe would be to break down the problem into smaller, manageable steps and seek support in finding a solution."
-//     }
-// ]
